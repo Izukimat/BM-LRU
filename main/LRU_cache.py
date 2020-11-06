f2a107724a30f43efa521027426e213d18c25ca3
Izzy Matt <izuki@Izzys-MacBook-Pro.local>
2020-11-05 23:51:47 -0500

    added delete and reset

--- main/LRU_cache.py
+++ main/LRU_cache.py
@@ -3,26 +3,29 @@ from collections import OrderedDict
 class LRUCache(object):
     def __init__(self, max_items):
         self.max_items = max_items
-        self.dict = OrderedDict()
+        self.cache = OrderedDict()
 
     def put(self, key, value):
-        if key in self.dict:
-            del self.dict[key]
-        elif len(self.dict) == self.max_items:
-            self.dict.popitem(False)
-        self.dict[key] = value
-        
+        if key in self.cache:
+            del self.cache[key]
+        elif len(self.cache) == self.max_items:
+            self.cache.popitem(False)
+        self.cache[key] = value
+
     def get(self, key):
-        if key in self.dict:
-            value = self.dict[key]
-            del self.dict[key]
-            self.dict[key] = value
+        if key in self.cache:
+            value = self.cache[key]
+            del self.cache[key]
+            self.cache[key] = value
             return value
         else:
             return None #TODO add error handling
 
     def delete(self, key):
-        pass
+        if key in self.cache:
+            del self.cache[key]
+        else:
+            pass
 
     def reset(self):
-        pass
+        self.cache.clear()
